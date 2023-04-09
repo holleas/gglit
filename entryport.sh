8fd0704cc450a4d66565a5d839532551faee207a
yhm326 <128949284+yhm326@users.noreply.github.com>
2023-04-08 21:15:52 -0800

    Update entryport.sh

--- entryport.sh
+++ entryport.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'f1914322-5e72-47eb-8e2c-880176f338c3'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
